23d8d54b721e351b0b5cbfd3b94929f04f3c2263
Hina0222 <hina0222@naver.com>
2026-03-06 22:08:43 +0900

    style: pretendard 폰트 적용

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import './globals.css';
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+});
 
 export const metadata: Metadata = {
   title: 'BRAGram',
@@ -12,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className="dark">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="ko">
+      <body className={`${pretendard.className} dark antialiased`}>{children}</body>
     </html>
   );
 }
