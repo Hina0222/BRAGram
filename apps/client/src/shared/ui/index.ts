30a58ee1a6aec9b568cd1058097a03b044f4bc7f
Hina0222 <hina0222@naver.com>
2026-03-28 20:00:06 +0900

    feat[client]: dropdown-menu 추가

--- apps/client/src/shared/ui/index.ts
+++ apps/client/src/shared/ui/index.ts
@@ -22,3 +22,20 @@ export {
   CarouselNext,
   CarouselPrevious,
 } from './carousel';
+export {
+  DropdownMenu,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+} from './dropdown-menu';
